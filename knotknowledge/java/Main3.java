7e82bc9c9ff7bbe4b3f36b307d3e1df1ce8362dc
Daniel Anker Hermansen <daniel201001@gmail.com>
2025-09-10 17:34:44 +0200

    move decelerating jump

---
+++ knotknowledge/java/Main3.java
@@ -0,0 +1,23 @@
+class Main3 {
+	private static void solve(Kattio io) {
+		int n = io.getInt();
+		int[] x = new int[n];
+		for (int i = 0; i < n; i++) x[i] = io.getInt();
+		int[] y = new int[n - 1];
+		for (int i = 0; i < n - 1; i++) y[i] = io.getInt();
+		for (int xi : x) {
+			boolean found = false;
+			for (int yi : y) {
+				if (xi == yi) found = true;
+			}
+			if (!found) io.println(xi);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Kattio io = new Kattio(System.in, System.out);
+		solve(io);
+		io.close();
+	}
+
+}
